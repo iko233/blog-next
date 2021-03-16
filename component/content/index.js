9faac1c4aa621a16a5d540d39cf7904253911fb5
bitchigo <1020142782@qq.com>
2021-03-16 17:00:39 +0800

    fix Pagination

--- component/content/index.js
+++ component/content/index.js
@@ -4,12 +4,14 @@ import {
   Pagination,
   Typography,
   Skeleton,
+  Tag,
 } from 'antd';
 import 'moment/locale/zh-cn';
 import 'antd/dist/antd.css';
 import styles from './index.module.css';
 import Axios from 'axios';
-import Link from 'next/link'
+import Link from 'next/link';
+import Router, { useRouter } from 'next/router';
 import {
 
 } from '@ant-design/icons';
@@ -18,28 +20,39 @@ import React from 'react';
 const { Content } = Layout;
 const { Title, Paragraph } = Typography;
 export default function MyContent() {
-
+  const router = useRouter();
   const [loading, setLoading] = React.useState(true);
   const [data, setData] = React.useState([]);
   const [total, setTotal] = React.useState(0);
-  const [current, setCurrent] = React.useState(1);
-  
 
   React.useEffect(() => {
-    //获取data
-    setLoading(true);
-    Axios({
-      url: '/api/public/posts/' + current,
-      method: 'GET'
-    }).then(rep => {
-      setTotal(rep.data.total);
-      setData(rep.data.homeCards);
-      setLoading(false);
-    });
-  }, [current]);
 
+    console.log(router);
+    if(router.route=='/'){
+      setLoading(true);
+      Axios({
+        url: '/api/public/posts/1',
+        method: 'GET'
+      }).then(rep => {
+        setTotal(rep.data.total);
+        setData(rep.data.homeCards);
+        setLoading(false);
+      });
+    }else if (router.asPath !== router.route) {
+      setLoading(true);
+      Axios({
+        url: '/api/public/posts/' + router.query.page,
+        method: 'GET'
+      }).then(rep => {
+        setTotal(rep.data.total);
+        setData(rep.data.homeCards);
+        setLoading(false);
+      });
+    }
+  }, [router])
   return (
     <Content className={styles.myContent}>
+
       {loading ?
         <div>
           <Card bordered={true} className={styles.myContentCard}>
@@ -97,6 +110,16 @@ export default function MyContent() {
             <Link href={'/posts/' + item.id}>
               <a >
                 <Card bordered={true} className={styles.myContentCard}>
+                  <div className={styles.myContentCardTag}>
+                    {item.target.map((tag) => {
+                      return (
+                        <Link href={'/class/' + tag.id}>
+                          <Tag color={tag.color}>{tag.name}</Tag>
+                        </Link>
+                      )
+                    })
+                    }
+                  </div>
                   <Title>{item.title}</Title>
                   <Paragraph>{item.content}</Paragraph>
                 </Card>
@@ -105,7 +128,8 @@ export default function MyContent() {
           )
         })
       }
-      < Pagination defaultCurrent={current} current={current} total={total} className={styles.myPagination} />
+      
+      < Pagination defaultCurrent={router.route=='/'?1:parseInt(router.query.page)} current={router.route=='/'?1:parseInt(router.query.page)} onChange={e => {  Router.push({ pathname: `/${e}` })}} total={total} className={styles.myPagination} />
     </Content >
   )
 
--- pages/[page].js
+++ pages/[page].js
@@ -6,24 +6,26 @@ import {
 import zhCN from 'antd/lib/locale/zh_CN';
 import 'moment/locale/zh-cn';
 import 'antd/dist/antd.css';
+
 import MyHeader from '../component/header';
 import MyContent from '../component/content';
 import MyAside from '../component/aside';
-
+import { createContext } from 'react';
 
 export default function Home() {
+  
+  const context=createContext();
   return (
     <div>
       <ConfigProvider locale={zhCN}>
         <Layout>
             <MyHeader key='1'/>    
           <div className={styles.mainBox}>
-            <MyContent/>
+            <MyContent myContext={context}/>
             <MyAside/>
           </div>
         </Layout>
       </ConfigProvider>
-      
     </div>
   )
 }
